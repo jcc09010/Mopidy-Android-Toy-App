b60ac11930049320f8c66d18873ba1a70fd6a4fb
Justin Chu <justin.chu@savantsystems.com>
2017-01-28 19:55:05 -0500

    Initial Project Setup

---
+++ app/src/main/java/com/sideprojects/work/mopifyandroid/BrowseFragment.java
@@ -0,0 +1,22 @@
+package com.sideprojects.work.mopifyandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by work on 1/28/17.
+ */
+
+public class BrowseFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_browse, container, false);
+        return root;
+    }
+}
